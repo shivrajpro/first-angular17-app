d3ba43a85e9d8ec0fb5a06f35bb3188d20e90231
shivrajhulbatte@gmail.com <shivrajhulbatte@gmail.com>
2025-01-11 20:08:44 +0530

    creating new task dialog

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,17 +2,19 @@ import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { dummyTasks } from '../dummy-tasks';
 import { Task } from './task/task.model';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) username!: string;
+  isAddingTask: boolean = false;
 
   tasks = dummyTasks;
 
@@ -22,5 +24,13 @@ export class TasksComponent {
 
   onCompleteTask(id: string) {
     this.tasks = this.tasks.filter((task: Task) => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  closeAddNewTaskDialog() {
+    this.isAddingTask = false;
   }
 }
